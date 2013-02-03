8e0072c386213378690fbb17cf94fa673a46957c
Wei <jrweizhang@gmail.com>
2013-02-03 11:26:32 +0800

    init and add venus, pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youboard::Application.config.secret_token = '03a0e6abd8af2b0197b12b7266a71366b29dec220a47160d8d110c3fb4cea5c521788feaa3a99e28a149a6448c1867b3b5d4a85f0b22913f20033b35a56f37d7'
