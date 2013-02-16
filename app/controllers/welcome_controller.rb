a195394c90b622e13b1c80879e406d1e75be7039
Wei <jrweizhang@gmail.com>
2013-02-16 13:48:14 +0800

    Adjust index page for subscribe other chart.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,14 +4,15 @@ class WelcomeController < ApplicationController
   end
 
   def subscribe
-    if current_user.find_or_create_playlist_by_hot_type(Setting.hot_type[1]).can_update?
+    hot_type = Setting.hot_type[params[:hot_type_id].to_i]
+    if current_user.find_or_create_playlist_by_hot_type(hot_type).can_update?
       args = {
         :user_id => current_user.id,
-        :type => Setting.hot_type[1],
+        :type => hot_type,
         :token => current_user.authorizations.find_by_provider('google_oauth2').get_token!
       }
       UpdateWorker.perform_async(args, 1) 
-      flash[:notice] = 'Pleas wait minutes for playlist updating.'
+      flash[:notice] = "Pleas wait minutes for #{hot_type.humanize} playlist updating."
     else
       flash[:notice] = 'We are updating your playlist, please wait.'
     end
